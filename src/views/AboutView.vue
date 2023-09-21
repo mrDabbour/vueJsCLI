72cab2a55f9c49aa491fde4be21c9e3d6ac8deb2
mrDabbour <jmomj@gmail.com>
2023-09-22 01:06:43 +0200

    start work with vuejs2 CLI adding some template

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <WelcomeMessage />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import WelcomeMessage from '@/components/WelcomeMessage.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    WelcomeMessage
+  }
+}
+</script>
+
