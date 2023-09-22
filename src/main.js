6ebdc739013069349258e7e8164bdcbf83e78b5a
mrDabbour <jmomj@gmail.com>
2023-09-22 18:51:26 +0200

    Adding Bootstrap and Dependecies

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './scss/main.scss';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/main.scss";
 import "normalize.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -2,6 +2,26 @@
   <div class="our-blog">
     <h1>{{ pageName }}</h1>
     <h3>{{ pageDescription }}</h3>
+
+    <!-- alert code  -->
+    <div class="container">
+      <div
+        v-if="showAlert"
+        class="alert alert-success alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>تعلم Vue.js:</strong> يمكنك إضافة رسالة هنا.
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          aria-label="Close"
+          @click="closeAlert"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,8 +31,33 @@ export default {
     return {
       pageName: "Blog",
       pageDescription: "This is blog page",
+      showAlert: true,
     };
+  },
+  methods: {
+    closeAlert() {
+      this.showAlert = false; // تعيين الحالة لإخفاء التنبيه عند النقر على الزر
+    },
   },
   name: "blog-view",
 };
 </script>
+
+<style scoped>
+.alert {
+  background-color: #d4edda;
+  color: #155724;
+  border-color: #c3e6cb;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+.alert button.close {
+  font-size: 20px;
+}
+
+.alert button.close:hover {
+  color: #155724;
+}
+</style>
