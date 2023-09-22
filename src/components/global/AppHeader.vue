8f3719614ca0b40a2f29505e85392073422662a6
mrDabbour <jmomj@gmail.com>
2023-09-22 18:12:58 +0200

    Add new package and component data

--- src/components/global/AppHeader.vue
+++ src/components/global/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <div class="logo">Mohammed Dabour</div>
-    <ul id="nav">
+  <div class="header">
+    <div class="logo">Mohammed Dabour Blog</div>
+    <ul>
       <router-link to="/" tag="li" exact><a>Home</a></router-link>
-      |
+      
       <router-link to="/about" tag="li"><a>About</a></router-link>
-      |
+      
       <router-link to="/help" tag="li" replace><a>Help</a></router-link>
     </ul>
   </div>
@@ -15,3 +15,29 @@ export default {
   name: "AppHeader",
 };
 </script>
+<style scoped lang="scss">
+.header {
+  background-color: #333;
+  color: #fff;
+  overflow: hidden;
+  .logo {
+    font-size: 24px;
+    padding: 20px;
+    float: left;
+    width: 200px;
+  }
+  ul {
+    float: left;
+    width: calc(100% - 200px);
+    li {
+      padding: 20px;
+      display: inline-block;
+      font-size:18px;
+      a {
+        color: #fff;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
